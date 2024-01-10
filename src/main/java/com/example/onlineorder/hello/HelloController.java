0ef59b2e908f2b698d304d2203cce808dabb8c32
Celestialfoxx <151230788+Celestialfoxx@users.noreply.github.com>
2024-01-10 14:22:45 -0500

    implementation of restful api
    use restful api , record, and json

---
+++ src/main/java/com/example/onlineorder/hello/HelloController.java
@@ -0,0 +1,29 @@
+package com.example.onlineorder.hello;
+
+import com.github.javafaker.Faker;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public Person sayHello() {
+        Faker faker = new Faker();
+        String name = faker.name().fullName();
+        String company = faker.company().name();
+        String street = faker.address().streetAddress();
+        String city = faker.address().city();
+        String state = faker.address().state();
+        String bookTitle = faker.book().title();
+        String bookAuthor = faker.book().author();
+        //String template = "This is %s. I work at %s. I live at %s in %s %s. My favorite book is %s by %s.";
+        return new Person(name, company, new Address(street, city, state, null), new Book(bookTitle, bookAuthor));
+
+    }
+
+    @GetMapping("/goodbye")
+    public String sayGoddbye() {
+        return "Goodbye!";
+    }
+
+}
